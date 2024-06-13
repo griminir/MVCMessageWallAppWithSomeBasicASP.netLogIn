84ed2afd10fa934988a6e56d291aad929957eaed
griminir <viktor.elliot.king@gmail.com>
2024-06-13 11:46:42 +0200

    Add project files.

---
+++ MVCMessageWall/Models/MessageModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCMessageWall.Models
+{
+    public class MessageModel
+    {
+        [Required]
+        [StringLength(10, MinimumLength = 5)]
+        [Display(Name = "really cool Message")]
+        public string Message { get; set; }
+    }
+}
---
+++ MVCMessageWall/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MVCMessageWall.Areas.Identity.Data;
+using MVCMessageWall.Models;
+namespace MVCMessageWall
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("MVCMessageWallContextConnection") ?? throw new InvalidOperationException("Connection string 'MVCMessageWallContextConnection' not found.");
+
+            builder.Services.AddDbContext<MVCMessageWallContext>(options => options.UseSqlServer(connectionString));
+
+            builder.Services.AddDefaultIdentity<MVCMessageWallUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<MVCMessageWallContext>();
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+
+           var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            app.UseRouting();
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
